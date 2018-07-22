2e6e62d4e869dc67c75c7e0b6e483b6cedc6dd4f
Rohan Paul <rohanpaul2@gmail.com>
2018-07-22 23:46:24 +0530

    exit from create-react-app and setting up some basic configuration

---
+++ src/containers/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+    <div>
+        <h2>About Page</h2>
+        <p>
+         This is About Page
+        </p>
+    </div>
+)
+
+export default About;
