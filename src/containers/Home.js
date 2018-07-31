702e6de6c0822439f91b5561b78d14fde5333069
Rohan Paul <rohanpaul2@gmail.com>
2018-07-31 19:10:20 +0530

    completing the main Container component Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -30,7 +30,20 @@ const mapStateToProps = state => ({
 
 })
 
-/* What mapStateToProps() does is, it allows us to take our counterValue state (from the root reducer ..reducers/index.js ) and turn this into a component property so I can   use it in this Home.js container component - like e.g.
+const mapDispatchToProps = dispatch => ({
+    handleIncreaseValue: () => {
+        dispatch(actions.addToCounter())
+    },
+    handleDecreaseValue: () => {
+        dispatch(actions.removeFromCounter())
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+
+
+/* What mapStateToProps() does is, it allows us to take our counterValue state (from the root reducer ..reducers/index.js ) and turn this into a component property so I can use it in this Home.js container component - like e.g.
 
 this.props.counterValue
 
@@ -38,5 +51,16 @@ mapStateToProps() has the Store state as an argument and its used to link the co
 
 < const Home = ({ counterValue, handleIncreaseValue, handleDecreaseValue }) => (  >
 
+mapStateToProps and mapDispatchToProps are both pure functions that are provided with the (or passed to them), the stores “state” and “dispatch” respectively. Furthermore, both functions have to return an object, whose keys will then be passed on as the props of the component they are connected to (Home.js in this case).
 
-mapStateToProps and mapDispatchToProps are both pure functions that are provided with the (or passed to them), the stores “state” and “dispatch” respectively. Furthermore, both functions have to return an object, whose keys will then be passed on as the props of the component they are connected to (Home.js in this case). */
+mapStateToProps() is a utility which helps your component get updated state(which is updated by some other components),
+
+mapDispatchToProps() is a utility which will help your component to fire an action event (dispatching action which may cause change of application state). we can remove all reference to our store from our component via the mapDispatchToProps() function. We saw that mapDispatchToProps() allows us to bring in actions, combine them with dispatch and connect events on our page to actions in our store.
+
+ mapDispatchToProps()  - just like with mapStateToProps() we added a prop that pointed to a value, here we add two props `handleIncreaseValue' and 'handleDecreaseValue' that point to values, which are two functions. The dispatch() function is available as an argument to mapDispatchToProps. By including the dispatch, we've bundled everything we need into two props value, that I am passing at the very top to this component.
+
+ < const Home = ({ counterValue, handleIncreaseValue, handleDecreaseValue }) => (  >
+
+**mapStateToProps and mapDispatchToProps** are separate for a good reason, consider the performance: mapStateToProps is actually run several times when state changes, and mapDispatchToProps once (or way fewer anyway than mapStateToProps) it doesn't depend on the state.
+
+*/
