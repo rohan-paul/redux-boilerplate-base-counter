1d5569e83c93a36047ba987fe2a85e69a60e2144
Rohan Paul <rohanpaul2@gmail.com>
2018-08-07 00:59:25 +0530

    coding Routes.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom'
+import Routes from './Routes'
 
 import './App.css';
 import About from './containers/About';
@@ -8,9 +9,17 @@ class App extends Component {
   render() {
     return (
         <BrowserRouter>
-            <div className="App">
-                <About />
+            <main className="container">
+            <div>
+                <h1>hellow wordl</h1>
             </div>
+            <ul className="left">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+            <Routes />
+            </main>
+
         </BrowserRouter>
     );
   }
