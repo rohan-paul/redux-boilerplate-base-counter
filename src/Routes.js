c53c32d087f5de3f9e1e7a32297be176ae65cda5
Rohan Paul <rohanpaul2@gmail.com>
2018-08-23 02:38:26 +0530

    trying to implement higher order component

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,11 @@ import { Route, Switch } from 'react-router-dom'
 
 import { Home, About } from './containers'
 
-const Routes = () => {
+const Routes = () => (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
     </Switch>
-}
+)
 
 export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// hot reload for development
+import {  AppContainer } from 'react-hot-loader'
+
+// redux deps
+import {  createStor, applyMiddleware } from 'redux'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+if (module.hot) {
+    module.hot.accepts('./App' () => {render(App) })
+    module.hot.accepts('./reducers', () => { store.replaceReducer(reducer) })
+}
+
+render(App);rker();
+
+
+
+if (module.hot) {
+    module.hot.accepts('./App', () => {render(App) })
+    module.hot.accepts('./reducers', () => { store.replaceReducer(reducer) })
+}
+
+render(App);
+
+/* What is repalaceReducer()
+
+Replaces the reducer currently used by the store to calculate the state. You might need this if your app implements code splitting, and you want to load some of the reducers dynamically. You might also need this if you implement a hot reloading mechanism for Redux. */
