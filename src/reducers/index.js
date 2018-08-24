ffca5284a65d299423d19377b8882963c631155f
Rohan Paul <rohanpaul2@gmail.com>
2018-08-24 18:11:52 +0530

    making the couter work for first time

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,4 @@ import { combineReducers } from 'redux';
 
 import counterReducer from './counterReducer'
 
-export default compbineReducers({ counterReducer })
+export default combineReducers({ counterReducer })
